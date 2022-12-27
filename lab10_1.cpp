09c77771931b0974415ae06f937c27948597b3a4
Napapon-Wongkom <dot.wongkom@gmail.com>
2022-12-27 16:59:15 +0700

    lab10_1.cpp

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main(){	
+	double loan,nb,rpy,payment;
+	int num=1;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rpy;
 	cout << "Enter amount you can pay per year: ";
-
+	cin >> payment;
+	
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +25,29 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	
+	while(true) {
+		double rpyint = (loan*rpy)/100.00;
+		double total = loan + rpyint;
+		if(total<payment){
+			payment = total;
+		}
+		nb=total-payment;
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << num; 
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << rpyint;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << payment;
+		cout << setw(13) << left << nb;
+		cout << "\n";
+		loan=nb;
+		num++;
+		if(nb==0){
+			break;
+		}
+	}
+		
 	
 	return 0;
 }
